67b113386c7b0c0e741ee594fc1dc6064ae632b8
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-10 11:41:03 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int count = 0;
+
+	while (count <= 9)
+	{
+		putchar('0' + count);
+		if (count != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
