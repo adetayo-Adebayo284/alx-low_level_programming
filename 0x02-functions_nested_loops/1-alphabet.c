f18514b649fb9a1a86ef9307714de834d6e52f3d
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-13 14:33:58 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - Check description
  *
  * DESCRIPTION: Write a function that prints the lower alphabet,
  *
- * Return: Always 0.
+ * Return: Always Nothing.
  */
 void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-	{
 		_putchar(i);
-	}
+
 	_putchar('\n');
 }
