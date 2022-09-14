34e2b03c374a82652913e9b0c0418c13a137e68c
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-14 04:02:46 -0700

    Task 6 Write a function that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_sign - a function that computes the absolute value of an integer.
+ * @n: input character to the function
+ * Returns: absolute value
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (-1 * n);
+}
