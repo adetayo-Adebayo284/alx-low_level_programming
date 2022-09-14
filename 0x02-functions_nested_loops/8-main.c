bb231190e4ba84e40fed261c0f6e5d2959dfd198
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-14 08:07:58 -0700

    Task 8 a function that prints every minute of the day of Jack Bauer, starting from 00:00 to

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * a function that checks for lowercase character.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
