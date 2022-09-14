6e0fcd7fe362758a7ba3d06de6d6f5c40f760b90
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-14 13:18:52 -0700

    Task 9 a function that prints the 9 times table, starting with 0.

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * a function that checks for lowercase character.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
