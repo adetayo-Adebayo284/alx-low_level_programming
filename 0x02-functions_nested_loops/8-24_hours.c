fdf9fb98e3e284223efa88ae990099e90cd90c3f
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-14 08:12:44 -0700

    Task 8 a function that prints every minute of the day of Jack Bauer, starting from 00:00 to

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,9 @@ void jack_bauer(void)
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
 			_putchar(':');
-			_putchar((M / 10) + '0');
+			_putchar((M / 10) + '0');	
 			_putchar((M % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
