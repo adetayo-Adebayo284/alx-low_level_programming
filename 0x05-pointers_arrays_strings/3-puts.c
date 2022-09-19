ecbae409d839c52afa39bf0d2a54dc5ccef37da4
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-19 10:39:33 -0700

    Task 3 - Write a function that prints a string, followed by a new line, to stdout.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout.
+ * @str: The parameter
+ * Return: Nothing.
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+	putchar('\n');
+}
