f3d1cf701d8b75a94fa0d68d460bbb05e852cc90
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-21 10:56:39 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * @*rot13 - a function that encodes a string using rot13.
+ * @str: An input string to encode using rot13.
+ * Return: An encode string
+ */
+
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		while ((str[i] >= 'a' && str[i] <= 'z') ||
+				(str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			if ((str[i] >= 'a' && str[i] <= 'm') ||
+					(str[i] >= 'A' && str[i] <= 'M'))
+			{
+				str[i] += 13;
+			}
+			else
+			{
+				str[i] -= 13;
+			}
+			i++;
+		}
+		i++;
+	}
+	return (str);
+}
