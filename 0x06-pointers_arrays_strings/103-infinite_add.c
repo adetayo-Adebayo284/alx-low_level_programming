9a929a47947fda023d9c767fb6368c784553d79c
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-22 05:08:41 -0700

    Task 11 - Write a function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,21 +12,21 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int num1 = 0, num2 = 0, op, bg, dr1, dr2, add = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (*(n1 + num1) != '\0')
+	while (*(n1 + c1) != '\0')
 		c1++;
-	while (*(n2 + num2) != '\0')
-		num2++;
-	if (num1 >= num2)
-		bg = num1;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
 	else
-		bg = num2;
+		bg = c2;
 	if (size_r <= bg + 1)
 		return (0);
 	r[bg + 1] = '\0';
-	num1--, num2--, size_r--;
-	dr1 = *(n1 + num1) - 48, dr2 = *(n2 + num2) - 48;
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 	while (bg >= 0)
 	{
 		op = dr1 + dr2 + add;
@@ -38,12 +38,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			*(r + bg) = (op % 10) + 48;
 		else
 			*(r + bg) = '0';
-		if (num1 > 0)
-			num1--, dr1 = *(n1 + num1) - 48;
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
 		else
 			dr1 = 0;
-		if (num2 > 0)
-			num2--, dr2 = *(n2 + num2) - 48;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
 		else
 			dr2 = 0;
 		bg--, size_r--;
