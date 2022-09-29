c7506bc324b7f0f7b5ec8e3b021dc62bd0c4aba7
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-28 18:47:28 -0700

    Task 7 - Write a function that returns 1 if a string is a palindrome and 0 if not.

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * pal - obtains length of a
+ * @a: string
+ * @l: integer to count length
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
+int pal(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (pal(a + 1, l + 1));
+}
+
+/**
+ * pali - compares string vs string reverse
+ * @a: string
+ * @l: length
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int pali(char *a, int l)
+{
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (pali(a + 1, l - 2));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_palindrome(char *s)
+{
+	int c;
+
+	c = pal(s, 0);
+	return (pali(s, l));
+}
