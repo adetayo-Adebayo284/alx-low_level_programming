4f2a9c870199977278623f146baaa3b408837053
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-28 18:20:12 -0700

    Task 6 -  a function that returns 1 if the input integer is a prime number, otherwise return 0.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * prime_ch - proto.
- * @f: input factor to check in proto.
- * @p: Possible prime number in proto.
- * Return: 1 if prime, else 0.
- */
-int prime_ch(int f, int p);
-
-/**
- * is_prime_number - States if number is prime
- * @n: Number to check
- * Return: 1 if prime, else 0.
- */
-int is_prime_number(int n)
-{
-	if (n == 2)
-		return (1);
-	return (prime_ch(2, n));
-}
-
-/**
  * prime_ch - Checks to see if number is prime
  * @f: input factor to check
  * @p: Possible prime number
@@ -34,5 +14,17 @@ int prime_ch(int f, int p)
 	else if (f > p / 2)
 		return (1);
 	else
-		return (prime_check(f + 1, p));
+		return (prime_ch(f + 1, p));
+}
+
+/**
+ * is_prime_number - States if number is prime
+ * @n: Number to check
+ * Return: 1 if prime, else 0.
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (prime_ch(2, n));
 }
