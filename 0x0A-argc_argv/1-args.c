04654cff8908cba5506fb9a32eb55400e32e29ca
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-09-30 12:03:09 -0700

    Task 1 - a program that prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main -  a program that prints the number of arguments passed into it.
+ * @argc: counts number of arguments
+ * @argv: array of argyments
+ * Return: zero, success
+ */
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
