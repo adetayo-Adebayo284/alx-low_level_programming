a356eadd6e5090fe9b73032731e97d9be2a03d41
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-10-04 06:00:09 -0700

    Task 0 - a function that creates an array of chars, and initializes it with a specific char.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_array - a function that creates an array of chars,
+ * and initializes it with a specific char.
+ * Returns NULL if size = 0
+ * Returns a pointer to the array, or NULL if it fails
+ *
+ * @size: Number of byte to allocated
+ * @c: array of character
+ * Return: string.
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *str;
+
+	if (size == 0)
+		return (NULL);
+	str = malloc(size * sizeof(char));
+
+	if (str == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
+}
