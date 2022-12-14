436e17942694da225b71cddc459d61488a2705c2
Adebayo Abdulrazak <adebayoadetayo284@gmail.com>
2022-12-14 10:17:11 -0800

    Task 0 modified

---
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * DESCRIPTION: Write a program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 0;
+	char arr[] = "_putchar";
+
+	for (i = 0; i < arr[i]; i++)
+	{
+		_putchar(arr[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,8 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,10 +24,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int div(int a, int b);
-int mod(int a, int b);
-extern div_t div(int __number, int __denom);
-#endif
+
+#endif /* _MAIN_H_ */
